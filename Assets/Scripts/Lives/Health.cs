ca14e022806a89f034bb7ad2849f9a8ea950814d
IanMunster <I.Munster92@gmail.com>
2017-01-11 10:26:49 +0100

    HitCollision1

---
+++ Assets/Scripts/Lives/Health.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Health.
+/// This script is responsible for the Health of Characters
+/// </summary>
+public class Health : MonoBehaviour {
+
+	[SerializeField] private float maxHealth;
+	private float _currentHealth;
+
+	public void SubtractHealth(float subtract){
+		_currentHealth -= subtract;
+	}
+
+	// Use this for initialization
+	void Start () {
+		_currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		print (this+" CurrentHealth: "+_currentHealth);
+
+		if(_currentHealth > maxHealth){
+			print ("CHEATER!");
+			_currentHealth = maxHealth;
+		}
+
+		if(_currentHealth == 0){
+			print (this +" IS DEAD");
+		}	
+	}
+}
---
+++ Assets/Scripts/Lives/HitCollider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HitCollider : MonoBehaviour {
+
+	[SerializeField] private UnityEvent hit;
+	[SerializeField] private string collider;
+	private bool _isHit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	bool OnTriggerEnter(Collider other){
+		if (other.CompareTag(collider)) {
+
+			print ("TRIGGER");
+
+			hit.Invoke ();
+			return _isHit = true;
+		} else {
+			return _isHit = false;
+		}
+	}
+}
