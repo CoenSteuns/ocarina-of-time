4c51ac02d9932363f519530e03e333a38c8093d4
IanMunster <I.Munster92@gmail.com>
2017-01-16 16:40:17 +0100

    TempUpload

--- Assets/Scripts/Camera/CameraAiming.cs
+++ Assets/Scripts/Camera/CameraAiming.cs
@@ -1,23 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Camera aiming.
+/// This Class is responsible for the Aiming of the Camera when on TARGET LOCK
+/// https://www.reddit.com/r/gamedev/comments/2scqxa/ocarina_of_times_ztargeting_mechanic/?st=ixua9kld&sh=710dc2a5     http://imgur.com/a/ivuhk
+/// or
+/// https://www.reddit.com/r/gamedev/comments/14lxsj/3d_camera_demonstration_implementation_and/
+/// </summary>
 public class CameraAiming : MonoBehaviour {
 	/*
-	//https://www.reddit.com/r/gamedev/comments/2scqxa/ocarina_of_times_ztargeting_mechanic/?st=ixua9kld&sh=710dc2a5
-	//http://imgur.com/a/ivuhk
-	//Check Distance From TARGET & PLAYER
-	//CenterPoint = Distance / 2 
-	//CenterPoint + Offset from PLAYER
-	//Get Angle from (CenterPoint & PLAYER)
-
-	//https://www.reddit.com/r/gamedev/comments/14lxsj/3d_camera_demonstration_implementation_and/
-
-	[SerializeField] private Transform target;
-	[SerializeField] private Transform player;
-	[SerializeField] private Vector3 offset;
-
-	private float distance;
+	 * Check Distance From TARGET & PLAYER
+	 * CenterPoint = Distance / 2
+	 * CenterPoint + Offset from PLAYER
+	 * Get Angle from (CenterPoint & PLAYER)
+	 */
 
 	// Use this for initialization
 	void Start () {
@@ -26,12 +23,7 @@ public class CameraAiming : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		distance = Vector2.Distance (target,player);
-		float centerpoint = distance / 2;
 
-		transform.LookAt (centerpoint);
-
-		float angle = Mathf.Atan2 (centerpoint, player);
 	}
-	*/
+
 }
