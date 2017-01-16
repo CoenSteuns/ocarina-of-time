cf96186b2c4937a1bc8b621a3947de6e91987fd0
IanMunster <I.Munster92@gmail.com>
2017-01-16 13:01:16 +0100

    CameraFollow1
    Added Prefabs & NewTestLevel

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Camera follow.
+/// 
+/// Following Video : @22.25  https://www.youtube.com/watch?v=qHdZ6JtKahk
+/// </summary>
+public class CameraFollow : MonoBehaviour {
+
+	[SerializeField] private GameObject target;
+	[SerializeField] private float minDistance;
+	[SerializeField] private float speed;
+
+	
+	// Update is called once per frame
+	void Update () {
+		float _distance = Vector3.Distance (target.transform.position, transform.position);
+
+		if(_distance < minDistance){
+			
+		}
+	}
+}
